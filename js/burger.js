3b57517e59426d3389c6159de2747b3d51303875
Andrey Karimov <AndreyKarimov@gmail.com>
2023-04-18 18:28:33 +0300

    fix bags in burger menu

--- js/burger.js
+++ js/burger.js
@@ -1,8 +1,9 @@
 const BURGER_OPENED_CLASSNAME = 'burger-menu_open';
 const BURGER_BTN_OPENED_CLASSNAME = 'burger-btn_open';
-const BODY_FIXED_CLASSNAME = 'body_fixed';
+const BODY_FIXED_CLASSNAME2 = 'body__fix';
+const BURGER_CONTENT_CLASSNAME = 'burger-menu__content_open'
 
-const bodyNode = document.querySelector('body');
+const bodyNode2 = document.querySelector('body');
 const burgerNode = document.querySelector('.js-burger');
 const burgerBtnNode = document.querySelector('.js-burger-btn');
 const burgerContentNode = document.querySelector('.js-burger__content')
@@ -19,6 +20,7 @@ burgerNode.addEventListener('click', (event) => {
 
 function toggleBurger() {
     burgerNode.classList.toggle(BURGER_OPENED_CLASSNAME);
-    bodyNode.classList.toggle(BODY_FIXED_CLASSNAME);
+    bodyNode2.classList.toggle(BODY_FIXED_CLASSNAME2);
     burgerBtnNode.classList.toggle(BURGER_BTN_OPENED_CLASSNAME);
+    burgerContentNode.classList.toggle(BURGER_CONTENT_CLASSNAME);
 }
