6e1c1a806251a50a4c7c31f6474118f03181f90f
Andrey Karimov <AndreyKarimov@gmail.com>
2023-04-19 16:08:19 +0300

    fix desighn by maket

--- js/burger.js
+++ js/burger.js
@@ -1,17 +1,18 @@
 const BURGER_OPENED_CLASSNAME = 'burger-menu_open';
 const BURGER_BTN_OPENED_CLASSNAME = 'burger-btn_open';
-const BODY_FIXED_CLASSNAME2 = 'body__fix';
+const BODY_FIXED_CLASSNAME_2 = 'body__fix';
 const BURGER_CONTENT_CLASSNAME = 'burger-menu__content_open'
 
 const bodyNode2 = document.querySelector('body');
 const burgerNode = document.querySelector('.js-burger');
 const burgerBtnNode = document.querySelector('.js-burger-btn');
 const burgerContentNode = document.querySelector('.js-burger__content')
+const burgerContactsNode = document.querySelector('.js-burger-menu__contacts')
 
 burgerBtnNode.addEventListener('click', toggleBurger);
 
 burgerNode.addEventListener('click', (event) => {
-    const isClickOutsideContent = !event.composedPath().includes(burgerContentNode)
+    const isClickOutsideContent = !event.composedPath().includes(burgerContactsNode)
 
     if (isClickOutsideContent) {
         toggleBurger();
@@ -20,7 +21,36 @@ burgerNode.addEventListener('click', (event) => {
 
 function toggleBurger() {
     burgerNode.classList.toggle(BURGER_OPENED_CLASSNAME);
-    bodyNode2.classList.toggle(BODY_FIXED_CLASSNAME2);
+    bodyNode2.classList.toggle(BODY_FIXED_CLASSNAME_2);
     burgerBtnNode.classList.toggle(BURGER_BTN_OPENED_CLASSNAME);
     burgerContentNode.classList.toggle(BURGER_CONTENT_CLASSNAME);
+   
 }
+
+
+// _________________________________________________________________________
+
+// const BURGER_OPENED_CLASSNAME = 'burger_open';
+// const BURGER_BTN_OPENED_CLASSNAME = 'burger-btn_open';
+// const BODY_FIXED_CLASSNAME = 'body_fixed';
+
+// const bodyNode = document.querySelector('body');
+// const burgerNode = document.querySelector('.js-burger');
+// const burgerBtnNode = document.querySelector('.js-burger-btn');
+// const burgerContentNode = document.querySelector('.js-burger__content')
+
+// burgerBtnNode.addEventListener('click', toggleBurger);
+
+// burgerNode.addEventListener('click', (event) => {
+//     const isClickOutsideContent = !event.composedPath().includes(burgerContentNode)
+
+//     if (isClickOutsideContent) {
+//         toggleBurger();
+//     }
+// })
+
+// function toggleBurger() {
+//     burgerNode.classList.toggle(BURGER_OPENED_CLASSNAME);
+//     bodyNode.classList.toggle(BODY_FIXED_CLASSNAME);
+//     burgerBtnNode.classList.toggle(BURGER_BTN_OPENED_CLASSNAME);
+// }
